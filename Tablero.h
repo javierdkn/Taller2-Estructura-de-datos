ad2b0e97ad60f457b207b32b245628cc53c815b7
javierdkn <javierdinamarcafredes@gmail.com>
2024-12-07 10:07:46 -0300

    update Tablero.h

--- Tablero.h
+++ Tablero.h
@@ -3,15 +3,23 @@
 
 
 class Tablero {
+
 private:
-    char tablero[3][3];
+    char tabla[3][3]; // Tablero de juego
+    int espaciosLibres; // Espacios disponibles en el tablero
+    char letraJugador; // Ficha del jugador
+    char letraCPU; // Ficha de la CPU
+
+    bool estadoTerminal();
+    bool fueraDeRango(int fila, int columna);
+    int minimax(bool turnoCPU);
 
 public:
     Tablero();
-    void mostrarTablero();
-    bool colocar(int fila, int columna, char simbolo);
-    bool verificarGanador();
-
+    void mostrarTabla();
+    void insertarFichaHumano();
+    void insertarFichaCPU();
+    void elegirFicha(char jugadorFicha);
 };
 
-#endif 
+#endif
