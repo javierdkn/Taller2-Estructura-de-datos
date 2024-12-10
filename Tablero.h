e1f918ab0954c4088e1b5e563455d075b2bb80e1
javierdkn <javierdinamarcafredes@gmail.com>
2024-12-09 22:52:32 -0300

    update Tablero.h

--- Tablero.h
+++ Tablero.h
@@ -6,13 +6,13 @@ class Tablero {
 
 private:
     char tabla[3][3]; 
-    int espaciosLibres; 
+    int espaciosLibres,alfa,beta; 
     char letraJugador; 
     char letraCPU; 
 
     bool estadoTerminal();
     bool fueraDeRango(int fila, int columna);
-    int minimax(bool turnoCPU);
+    int minimax(bool turnoCPU, int alfa, int beta);
 
 public:
     Tablero();
